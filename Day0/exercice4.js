1e2d629ba5d8753ae8070d39c2a24169f8e87afc
elmahfoudinina-crypto <elmahfoudinina@gmail.com>
2026-09-17 14:46:47 +0100

    exercice4.js

---
+++ Day0/exercice4.js
@@ -0,0 +1,17 @@
+let score=95
+if(score>=90 && score<=100)
+    console.log("A")
+else if (score>=80 && score<=89)
+     console.log("B") 
+else if (score>=70 && score<=79)
+    console.log("c")
+else if (score>=70 && score<=0)
+      console.log("F")
+else 
+    console.log(error)
+
+    
+
+
+
+
