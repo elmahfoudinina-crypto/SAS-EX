b06e2cce2bdcca2bc10e5b71a78b60b4ac9c518f
elmahfoudinina-crypto <elmahfoudinina@gmail.com>
2026-09-19 08:33:28 +0100

    .\exercice3.js

--- Day1/exercice2.js
+++ Day1/exercice2.js
@@ -2,4 +2,4 @@ let number = 0
  for(let i = 1 ; i <= 100 ; i++) {
     number = number + i 
  }
-  console.log(number)
+  console.log(number) 
---
+++ Day1/exercice3.js
@@ -0,0 +1,3 @@
+let number = 5 
+  for(let i = 0 ; i <= 10 ; i++){
+    console.log(number + " + "  + i + " = " + number*i)  }
